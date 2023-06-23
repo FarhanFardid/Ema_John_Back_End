d8d2aa5a2918db28cf6264ae013b8f3d1ded828d
FarhanFardid <farhanfardid009@gmail.com>
2023-06-23 22:44:32 +0600

    db collection created

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
      client.connect();
+
+     const productCollection = client.db("EmaJohnDB").collection('products'); 
+
+     app.get('/products', async (req,res)=>{
+        const result = await productCollection.find().toArray();
+        res.send(result);
+     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
